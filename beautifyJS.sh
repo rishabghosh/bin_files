dc39f14f97365ac792925427a0fe427b6b51cff6
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-11-24 16:47:26 +0530

    source_file contains  (source file name)

--- beautifyJS.sh
+++ beautifyJS.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+source_file=$1
+
 #install js-beautify -> sudo npm install -g js-beautify
-js-beautify -b preserve-inline $1 > .beautified
-mv .beautified $1
+js-beautify -b preserve-inline $source_file > .beautified
+mv .beautified $source_file
 
 message="${LIGHTGRAY} Plz run test to see if something is broken"
 GREEN='\033[0;32m'
@@ -10,14 +12,14 @@ LIGHTGRAY='\033[0;37m'
 NC='\033[0m'
 
 #grep --version should be 2.10 or above
-echo "$1" | grep -io "test"
+echo "$source_file" | grep -io "test"
 result=$?
 
 if [ $result = 0 ]; then 
-  mocha $1
-  message="${LIGHTGRAY} mocha tested -> $1"
+  mocha $source_file
+  message="${LIGHTGRAY} mocha tested -> $source_file"
 fi
 
-echo -e "${GREEN} $1 ${LIGHTGRAY} is beautified"
+echo -e "${GREEN} $source_file ${LIGHTGRAY} is beautified"
 echo -e "$message"
 
