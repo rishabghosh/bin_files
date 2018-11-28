55287dc284d9f9104788a81c235bb5c9b0dabea7
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-11-28 23:31:32 +0530

    git_ready.sh makes pre-commit files executable

--- git_ready.sh
+++ git_ready.sh
@@ -2,6 +2,7 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m'
+RED='\033[0;31m'
 
 #initailzing a directory multiple times wont do any harm
 git init .
@@ -15,9 +16,10 @@ echo '{"esversion": 6}' > .jshintrc
 
 echo "mocha" >     .git/hooks/pre-commit
 echo "jshint ." >> .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
 
 echo ""
 echo -e "${GREEN}.gitignore"
 echo -e "${GREEN}.jshintrc"
-echo -e "${NC}.git/hooks/${GREEN}pre-commit ${NC}created"
+echo -e "${NC}.git/hooks/${RED}pre-commit ${NC}created"
 
