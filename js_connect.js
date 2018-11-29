88b54f24494f05b5e2afe37301f0892a2ac81718
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-11-29 14:22:55 +0530

    Connects to the JS community
    opens multiple web address in default browser
    opens Notes

---
+++ js_connect.js
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+open https://www.facebook.com/groups/HTML.CSSandJavaScript/
+open https://www.facebook.com/groups/1517091958315927/
+open https://www.facebook.com/groups/JavaScript.Programming/
+open https://www.facebook.com/groups/programmershub1/
+
+open -a "Notes"
+ 
